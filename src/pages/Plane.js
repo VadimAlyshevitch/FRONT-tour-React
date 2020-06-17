4c080fbbf7121fbee25ea78ac3a2b7e2d828fc66
Vadim <vadimpwnz228@gmail.com>
2020-06-17 21:07:18 +0300

    add plane page out avia

--- src/pages/Plane.js
+++ src/pages/Plane.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Hero from '../components/Hero'
 import Banner from '../components/Banner'
 import {Link} from 'react-router-dom'
@@ -6,237 +6,24 @@ import RoomContainer from '../components/RoomContainer';
 import StyledFooter from '../components/StyledFooter'
 
 
-
-
-
-
-
 // //онлайн база городов, прокси, ключ АПИ и БД по календарю цен
 const CITY_API = 'http://api.travelpayouts.com/data/ru/cities.json',
 PROXY = 'https://cors-anywhere.herokuapp.com/',
 API_KEY = '5d2e3c92f5cbb5b20d19b432ed48fb95',
+
 	// Получение минимальных цен на перелёт для указанных даты вылета и городов вылета и назначения
 CALENDAR = 'http://min-prices.aviasales.ru/calendar_preload',
+
 MAX_COUNT = 2,  // количество карточек самых дешевых билетов
 HOTEL_API = 'http://engine.hotellook.com/api/v2/lookup.json'
 
-let city = [];
 
+const getCitiesApi = () => fetch(PROXY + CITY_API).then(res => res.json())
 
-const getData = (url, callback, reject = console.error) => {
-	try {
+const getTicketsApi = (cityFrom, cityTo, date) => fetch()
 
-		const request = new XMLHttpRequest();
-
-		request.open('GET', url);
-
-		request.addEventListener('readystatechange', () => {
-			if (request.readyState !== 4) return;
-
-			if (request.status === 200) {
-				callback(request.response);
-			} else {
-				reject(request.status);
-			}
-		});
-
-		request.send();
-	} catch (e) {
-		console.log(e);
-	}
-}
-
-// const showCity = (input, list) => {
-// 	list.textContent = '';
-
-// 	if (input.value !== '') {
-
-// 		cheapestTicket.style.display = 'none'
-// 		otherCheapTickets.style.display = 'none'
-
-// 		const filterCity = city.filter((item) => {
-// 			const fixItem = item.name.toLowerCase();
-// 			return fixItem.startsWith(input.value.toLowerCase());
-// 			// return fixItem.includes(input.value.toLowerCase());
-// 		});
-
-
-// 		if (filterCity.length === 0) {
-// 			const li = document.createElement('li');
-// 			li.classList.add('dropdown__city', 'error');
-// 			li.textContent = 'Такого города нет'
-// 			list.append(li);
-// 		} else {
-// 			filterCity.forEach((item) => {
-// 				const li = document.createElement('li');
-// 				li.classList.add('dropdown__city');
-// 				li.textContent = item.name;
-// 				list.append(li);
-// 			})
-// 		};
-// 		return;
-// 	}
-// };
-
-// const selectCity = (event, input, list) => {
-// 	const target = event.target;
-// 	if (target.tagName.toLowerCase() === 'li') {
-// 		input.value = target.textContent;
-// 		list.textContent = '';
-// 	}
-// }
-
-// const getNameCity = (code) => {
-// 	const objCity = city.find(item => item.code === code)
-// 	return objCity.name
-// }
-
-// const getDate = (date) => {
-// 	return new Date(date).toLocaleDateString('ru-Ru', {
-// 		year: 'numeric',
-// 		month: 'long',
-// 		day: 'numeric',
-// 		hour: 'numeric',
-// 		minute: 'numeric',
-// 		timeZoneName: 'short'
-// 	})
-
-// }
-
-// const getChanges = (num) => {
-// 	if (num) {
-// 		return num === 1 ? 'С одной пересадкой' : 'С двумя пересадками'
-// 	} else {
-// 		return 'Без пересадок'
-// 	}
-// }
-
-// const getLinkAviasales = (data) => {
-// 	let link = 'https://www.aviasales.ru/search/'
-// 	link += data.origin
-
-// 	const date = new Date(data.depart_date)
-// 	const day = date.getDate()
-// 	link += day < 10 ? '0' + day : day
-// 	const month = date.getMonth() + 1
-// 	link += month < 10 ? '0' + month : month
-
-// 	link += data.destination
-// 	link += '1'   // один взрослый билет
-
-// 	return link
-// }
-
-// const createCard = (data) => {
-// 	const ticket = document.createElement('article')
-// 	ticket.classList.add('ticket')
-// 	let deep = ''
-// 	if (data) {
-// 		deep = `
-// 			<h3 class="agent">${data.gate}</h3>
-// 			<div class="ticket__wrapper">
-// 				<div class="left-side">
-// 					<a href="${getLinkAviasales(data)}" target="_blank" class="button button__buy">Купить
-// 						за ${data.value}₽</a>
-// 				</div>
-// 				<div class="right-side">
-// 					<div class="block-left">
-// 						<div class="city__from">Вылет из города
-// 							<span class="city__name">${getNameCity(data.origin)}</span>
-// 						</div>
-// 						<div class="date">${getDate(data.depart_date)}</div>
-// 					</div>
-
-// 					<div class="block-right">
-// 						<div class="changes">${getChanges(data.number_of_changes)}</div>
-// 						<div class="city__to">Город назначения:
-// 							<span class="city__name">${getNameCity(data.destination)}</span>
-// 						</div>
-// 					</div>
-// 				</div>
-// 			</div>`
-// 	} else {
-// 		deep = '<h3>К сожалением на указанную вами дату билетов нет</h3>'
-// 	}
-// 	ticket.insertAdjacentHTML('afterbegin', deep)
-// 	return ticket
-// }
-
-// const renderCheapDay = (cheapTicket) => {
-// 	cheapestTicket.style.display = 'block'
-// 	cheapestTicket.innerHTML = '<h2>Самый дешевый билет на выбранную дату</h2>'
-// 	const ticket = createCard(cheapTicket[0])
-// 	cheapestTicket.insertAdjacentElement('beforeend', ticket)
-// }
-
-// const renderCheapYear = (cheapTickets) => {
-// 	otherCheapTickets.style.display = 'block'
-// 	otherCheapTickets.innerHTML = '<h2>Самые дешевые билеты на другие даты</h2>'
-// 	cheapTickets.sort((a, b) => a.value - b.value)
-
-// 	for (let i = 0; i < cheapTickets.length && i < MAX_COUNT; i++) {
-// 		const ticket = createCard(cheapTickets[i])
-// 		otherCheapTickets.append(ticket)
-// 	}
-// }
-
-// const renderCheap = (data, date) => {
-// 	const cheapTicketYear = JSON.parse(data).best_prices
-// 	const cheapTicketDay = cheapTicketYear.filter((item) => { return item.depart_date === date })
-
-// 	renderCheapDay(cheapTicketDay)
-// 	renderCheapYear(cheapTicketYear)
-// }
-
-
-// inputCitiesFrom.addEventListener('input', () => {
-// 	showCity(inputCitiesFrom, dropdownCitiesFrom)
-// });
-
-// inputCitiesTo.addEventListener('input', () => {
-// 	showCity(inputCitiesTo, dropdownCitiesTo)
-// });
-
-// dropdownCitiesFrom.addEventListener('click', () => {
-// 	selectCity(event, inputCitiesFrom, dropdownCitiesFrom);
-// });
-
-// dropdownCitiesTo.addEventListener('click', () => {
-// 	selectCity(event, inputCitiesTo, dropdownCitiesTo);
-// });
-
-// body.addEventListener('click', () => {
-// 	dropdownCitiesFrom.innerHTML = '';
-// 	dropdownCitiesTo.innerHTML = '';
-// });
-
-// formSearch.addEventListener('submit', (event) => {
-// 	event.preventDefault()
-
-// 	const cityFromCode = city.find((item) => inputCitiesFrom.value === item.name).code
-// 	const cityToCode = city.find((item) => inputCitiesTo.value === item.name).code
-
-// 	const formData = {
-// 		from: cityFromCode,
-// 		to: cityToCode,
-// 		when: inputDateDepart.value
-// 	}
-
-// 	const requestData = CALENDAR +
-// 		`?depart_date=${formData.when}&origin=${formData.from}&destination=${formData.to}&one_way=true&token=${API_KEY}`;
-
-// 	getData(requestData, response => {
-// 		renderCheap(response, formData.when)
-// 	}, (error) => {
-// 		cheapestTicket.style.display = 'block'
-// 		cheapestTicket.innerHTML = '<h2 class ="error">В этом направлении нет рейсов</h2>'
-// 		console.error('Ошибка ', error)
-// 	})
-// })
-
-
-
-getData(PROXY + CITY_API, (data) => {
+/*
+getData(1, (data) => {
 	// выбираем только те объекты у которых не пустое поле name
 	city = JSON.parse(data).filter(item => item.name);
 
@@ -251,10 +38,65 @@ getData(PROXY + CITY_API, (data) => {
 	})
 
 });
-
+*/
 
 
 export default function Plane() {
+    const [data, setData] = useState([])
+    const [isFromSelectOpened, setIsFromSelectOpened] = useState(false)
+    const [isToSelectOpened, setIsToSelectOpened] = useState(false)
+    
+    const [fromValue, setFromValue] = useState('')
+    const [toValue, setToValue] = useState('')
+
+    const [selectedFromElem, setSelectedFromElem] = useState(null)
+    const [selectedToElem, setSelectedToElem] = useState(null)
+    const [date, setDate] = useState('')
+
+    const [tickets, setTickets] = useState([])
+    const [bestTicket, setBestTicket] = useState(null)
+
+    useEffect(() => {
+      getCitiesApi().then(data => setData(data.filter(elem => elem.name)))
+    }, [])
+
+    const getCityNameByCode = code => data.find(item => item.code === code).name
+
+    const formatDate = (date) => {
+      return new Date(date).toLocaleDateString('ru-Ru', {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+        hour: 'numeric',
+        minute: 'numeric',
+        timeZoneName: 'short'
+      }).toString()
+    }
+
+    const getChanges = (num) => {
+      if (num) {
+        return num === 1 ? 'С одной пересадкой' : 'С двумя пересадками'
+      } else {
+        return 'Без пересадок'
+      }
+    }
+
+    const getLinkAviasales = (data) => {
+      let link = 'https://www.aviasales.ru/search/'
+      link += data.origin
+    
+      const date = new Date(data.depart_date)
+      const day = date.getDate()
+      link += day < 10 ? '0' + day : day
+      const month = date.getMonth() + 1
+      link += month < 10 ? '0' + month : month
+    
+      link += data.destination
+      link += '1'   // один взрослый билет
+    
+      return link
+    }
+
     return (
         <>
             <Hero hero="roomsHero" >
@@ -265,26 +107,92 @@ export default function Plane() {
 
             <main>
                 <section className="wrapper">
-                  <form className="form-search">
+                  <form className="form-search" onSubmit={e => {
+
+                    e.preventDefault()
+                    if (!selectedToElem || !selectedToElem || !date) return
+
+                    fetch(CALENDAR + `?depart_date=${date}&origin=${selectedFromElem.code}&destination=${selectedToElem.code}&one_way=true&token=${API_KEY}`)
+                      .then(res => res.json())
+                      .then(data => {
+                        const best_prices = data.best_prices
+                        const bestTicket = best_prices.filter(item => item.depart_date === date)[0]
+                        setBestTicket(bestTicket)
+                        setTickets(best_prices.filter(item => item !== bestTicket))
+                        debugger
+                      })
+                  
+                  }}>
                     <div className="wrapper__search">
                       <div className="input" style={{ display: "block" }}>
                         <label>
                           Откуда
-                          <input type="text" className="input__cities-from" required />
+                          <input value={fromValue} onChange={e => {
+                            setFromValue(e.target.value)
+                            setSelectedFromElem(null)
+                          }}  type="text" className="input__cities-from" required onFocus={() => {
+                            setIsFromSelectOpened(true)
+                          }} onBlur={() => {
+                            setTimeout(() => {
+                              setIsFromSelectOpened(false)
+                            }, 200)
+                          }} />
                         </label>
-                        <ul className="dropdown dropdown__cities-from" />
+                        <ul className="dropdown dropdown__cities-from">
+                          {
+                            (isFromSelectOpened && fromValue) && data
+                              .filter(elem => {
+                                return elem.name.toLowerCase().startsWith(fromValue.toLowerCase())
+                              })
+                              .map(elem => {
+                                return <li className="dropdown__city" onClick={() => {
+                                  setSelectedFromElem(elem)
+                                  setFromValue(elem.name)
+                                }}>
+                                  {elem.name}
+                                </li>
+                              })
+                          }
+                        </ul>
                       </div>
                       <div className="input">
                         <label>
                           Куда
-                          <input type="text" className="input__cities-to" required />
+                          <input value={toValue} onChange={e => {
+                            setToValue(e.target.value)
+                            setSelectedToElem(null)
+                          }} onFocus={() => setIsToSelectOpened(true)}
+                          onBlur={() => {
+                            setTimeout(() => {
+                              setIsToSelectOpened(false)
+                            }, 200)
+                          }}
+                          type="text" className="input__cities-to" required />
                         </label>
-                        <ul className="dropdown dropdown__cities-to" />
+                        <ul className="dropdown dropdown__cities-from" onClick={e => {
+                          e.preventDefault()
+                          e.stopPropagation()
+                        }}>
+                          {
+                            (isToSelectOpened && toValue) && data
+                              .filter(elem => {
+                                return elem.name.toLowerCase().startsWith(toValue.toLowerCase())
+                              })
+                              .map(elem => {
+                                return <li className="dropdown__city" onClick={() => {
+                                  setSelectedToElem(elem)
+                                  setToValue(elem.name)
+                                }}>
+                                  {elem.name}
+                                </li>
+                              })
+                          }
+                        </ul>
                       </div>
                       <div className="input input__cities-from">
                         <label>
                           Отправление
-                          <input type="date" className="input__date-depart" required />
+                          <input value={date} onChange={(e) => setDate(e.target.value)} type="date" className="input__date-depart" required />
                         </label>
                       </div>
                     </div>
@@ -307,32 +215,56 @@ export default function Plane() {
                     style={{ display: "none" }}
                   ></section>
                 </section>
-      {/* <div>
-        <h3 className="agent">{data.gate}</h3>
-          <div className="ticket__wrapper">
-              <div className="left-side">
-                  <a href="${getLinkAviasales(data)}" target="_blank" className="button button__buy">Купить
-                      за&nbsp;{data.value}₽</a>
+                {
+                  bestTicket && <div>
+                  <h3 className="agent">{ bestTicket.gate }</h3>
+                    <div className="ticket__wrapper">
+                        <div className="left-side">
+                            <a href={getLinkAviasales(bestTicket)} target="_blank" className="button button__buy">Купить за&nbsp;{ bestTicket.value }₽</a>
+                          </div>
+                        <div className="right-side">
+                            <div className="block-left">
+                                <div className="city__from">Вылет из города
+                                  <span className="city__name">{ getCityNameByCode(bestTicket.origin) }</span>
+                                </div>
+                                <div className="date">{ formatDate(bestTicket.depart_date) }</div>
+                              </div>
+                            <div className="block-right">
+                                  <div className="changes">{ getChanges(bestTicket.number_of_changes) }</div>
+                                  <div className="city__to">Город назначения:
+                                  <span className="city__name">{ getCityNameByCode(bestTicket.destination) }</span>
+                                  </div>
+                                  </div>
+                              </div>
+                        </div>
                 </div>
-              <div className="right-side">
-                  <div className="block-left">
-                      <div className="city__from">Вылет из города
-                          <span className="city__name">{getNameCity(data.origin)}</span>
-                        </div>
-                      <div className="date">{getDate(data.depart_date)}</div>
-                    </div>
-                  <div className="block-right">
-                      <div className="changes">{getChanges(data.number_of_changes)}</div>
-                        <div className="city__to">Город назначения:
-                          <span className="city__name">{getNameCity(data.destination)}</span>
-                        </div>
-                        </div>
-                    </div>
-              </div>
-      </div> */}
-
-
-
+                }
+                {
+                  (tickets || []).filter((e, i) => i < MAX_COUNT).map(ticket => {
+                    return <div>
+                    <h3 className="agent">{ ticket.gate }</h3>
+                      <div className="ticket__wrapper">
+                          <div className="left-side">
+                              <a href={getLinkAviasales(ticket)} target="_blank" className="button button__buy">Купить за&nbsp;{ ticket.value }₽</a>
+                            </div>
+                          <div className="right-side">
+                              <div className="block-left">
+                                  <div className="city__from">Вылет из города
+                                    <span className="city__name">{ getCityNameByCode(ticket.origin) }</span>
+                                  </div>
+                                  <div className="date">{ formatDate(ticket.depart_date) }</div>
+                                </div>
+                              <div className="block-right">
+                                    <div className="changes">{ getChanges(ticket.number_of_changes) }</div>
+                                    <div className="city__to">Город назначения:
+                                    <span className="city__name">{ getCityNameByCode(ticket.destination) }</span>
+                                    </div>
+                                    </div>
+                                </div>
+                          </div>
+                  </div>
+                  })
+                }
               </main>;
 
               <StyledFooter /> 
